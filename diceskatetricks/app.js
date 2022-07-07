6580c734cc1abfa99fd5d9b1eb841ee8001ab07a
Facundo Volpi <fvolpi92@gmail.com>
2022-07-07 17:19:12 -0300

    Casi la primera version de "Random Skate Tricks"

---
+++ diceskatetricks/app.js
@@ -0,0 +1,11 @@
+//Guardando los lugares donde tengo que renderizar cada cosa individualmente + el boton
+const stanceElemento = document.querySelector('.postura');
+const direccionElemento = document.querySelector('.direccion');
+const gradosRotacionElemento = document.querySelector('.rotacion');
+const trickElemento = document.querySelector('.prueba');
+const btnElemento = document.querySelector('.fa4-butt');
+
+//Listas de pruebas, posición, dirección de giro, grados de rotación
+
+
+//Haciendo la funcion para randomizar los numeros
